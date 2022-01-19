8e471331786cc9b38ab0936b40c782de1ff6b6b0
Tamas Stirling <stirling.tamas@gmail.com>
2022-01-19 10:54:24 +0100

    Add new function for accessing biosample metadata with documentation and tests

---
+++ tests/testthat/test-ncbi_meta.R
@@ -0,0 +1,14 @@
+test_that("ncbi_meta() works with BioSample ID-s", {
+  biosample_uid <- get_uid("SAMN02714232", db = "biosample")
+  res <- ncbi_meta(biosample_uid)
+  
+  expect_s3_class(res, c("tbl_df", "tbl", "data.frame"))
+})
+
+test_that("ncbi_meta() works with BioSample UID-s", {
+  assembly_uid <- get_uid("GCF_000695855.3")
+  biosample_uid <- link_uids(assembly_uid, from = "assembly", to = "biosample")
+  res <- ncbi_meta(biosample_uid)
+  
+  expect_s3_class(res, c("tbl_df", "tbl", "data.frame"))
+})
