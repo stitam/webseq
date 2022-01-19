30f4a5803c77ae95788effca8111e8b88a9052d4
Tamas Stirling <stirling.tamas@gmail.com>
2022-01-19 21:56:51 +0100

    Update function for downloading genomes

--- R/download_genome.R
+++ R/download_genome.R
@@ -1,8 +1,7 @@
 #' Download Genomes from NCBI
 #'
-#' This function directly accesses a genome through the NCBI FTP server.
-#' @param assembly_uid numeric; a unique identifier. Use \code{get_uid} to
-#' acquire a unique UID.
+#' This function directly downloads genome data through the NCBI FTP server.
+#' @param accession character; a character vector of assembly accessions.
 #' @param type character; the file extension to download. Valid options are
 #' \code{"assembly_report"}, \code{"assembly_stats"}, \code{"cds"},
 #' \code{"feature_count"}, \code{"feature_table"}, \code{"genomic.fna"},
@@ -10,79 +9,93 @@
 #' \code{"protein.faa"}, \code{"protein.gpff"}, \code{"translated_cds"}.
 #' @param dirpath character; the path to the directory where the file should be
 #' downloaded.
-#' @param from character; either \code{"refseq"} or \code{"genbank"}.
-#' If \code{from} is set to \code{"refseq"} then the function will attempt to
-#' download the file from the refseq repository and use \code{"genbank"} as a
-#' fallback option.
-#' @param verbose boolean; should verbose messages be printed to console?
-#' @note ToDo: vectorise and add input validation to ensure function fails early
-#' if there are input errors. Add tests.
+#' @param verbose logical; should verbose messages be printed to console?
 #' @examples
 #' \dontrun{
-#' # Aim: download genbank file for "GCF_003007635.1"
-#'
+#' # Download genbank file for GCF_003007635.1.
 #' # The function will access files within this directory:
 #' # ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/007/635/
-#'
-#' # Step 1 - Get NCBI Assembly UID
-#' uid <- get_uid("GCF_003007635.1", db = "assembly")
-#'
-#' # Step 2 - Download file
-#' download_genome(uid, type = "genomic.gbff")
+#' download_genome("GCF_003007635.1", type = "genomic.gbff", verbose = TRUE)
+#' 
+#' # Download multiple files
+#' accessions <- c("GCF_000248195.1", "GCF_000695855.3")
+#' download_genome(accessions, type = "genomic.gbff", verbose = TRUE)
 #' }
-
-download_genome <- function(assembly_uid,
+#' @export
+download_genome <- function(accessions,
                             type = "genomic.gbff",
                             dirpath = NULL,
-                            from = "refseq",
                             verbose = getOption("verbose")) {
-  assembly_uid <- as.numeric(assembly_uid)
-  if (is.na(assembly_uid)) {
-    stop("Query is not an NCBI Assembly UID.")
-  }
   type <- match.arg(type, c(
     "assembly_report", "assembly_stats", "cds", "feature_count",
     "feature_table", "genomic.fna", "genomic.gbff", "genomic.gff",
     "genomic.gtf", "protein.faa", "protein.gpff", "translated_cds"))
-  from <- match.arg(from, c("refseq", "genbank"))
-  data <- rentrez::entrez_fetch(
-    "assembly", id = assembly_uid, rettype = "docsum", retmode = "json")
-  data <- jsonlite::fromJSON(data)
-  if (from == "refseq") {
-    ftppath <- paste0(data$result[[2]]$ftppath_refseq)
-    if (is.null(ftppath) | ftppath == "") {
-      if (verbose) message("Not found in RefSeq. Trying GenBank.")
-      ftppath <- paste0(data$result[[2]]$ftppath_genbank)
+  foo <- function(x, type) {
+    if (grepl("^GC[A|F]_[0-9]+\\.[0-9]+", x) == FALSE) {
+      if (verbose) {
+        message(x, ". Failed. Not an assembly accession.")
+      }
+      return(NA)
     }
-  }
-  prefix <- strsplit(ftppath, "/")[[1]]
-  prefix <- prefix[length(prefix)]
-  suffix <- switch(type,
-                   assembly_report = ".txt",
-                   assembly_stats = ".txt",
-                   cds.fna = "_from_genomic.fna.gz",
-                   feature_count = ".txt.gz",
-                   feature_table = ".txt.gz",
-                   genomic.fna = ".gz",
-                   genomic.gbff = ".gz",
-                   genomic.gff = ".gz",
-                   genomic.gtf = ".gz",
-                   protein.faa = ".gz",
-                   protein.gpff = ".gz",
-                   translated_cds = ".faa.gz")
-  urlpath <- paste0(ftppath, "/", prefix, "_" ,type, suffix)
-  if (is.null(dirpath)) {
-    dirpath = tempdir()
-  }
-  if (!dir.exists(dirpath)) dir.create(dirpath, recursive = TRUE)
-  filepath <- paste0(dirpath, "/", basename(urlpath))
-  if (file.exists(filepath)) {
-    message("Already downloaded: ", basename(urlpath))
-  } else {
-    out <- try(download.file(urlpath, destfile = filepath), silent = TRUE)
+    if (verbose) message(x, ". ", appendLF = FALSE)
+    assembly_uid <- try(get_uid(x, db = "assembly"), silent = TRUE)
+    if (inherits(assembly_uid, "try-error")) {
+      if (verbose) message("Failed. Webservice temporarily down.")
+      return(NA)
+    }
+    if (length(assembly_uid) == 1 && is.na(assembly_uid)) {
+      if (verbose) message("Failed. Assembly accession not found.")
+      return(NA)
+    }
+    if (length(assembly_uid) > 1) {
+      if (verbose) {
+        message("Failed. Multiple assembly uid-s found: ",
+                paste(assembly_uid, collapse = ", "), ".") 
+      }
+      return(NA)
+    }
+    assembly_meta <- try(ncbi_meta_assembly(assembly_uid), silent = TRUE)
+    if (inherits(assembly_meta, "try-error")) {
+      if (verbose) message("Failed. Webservice temporarily down.")
+      return(NA)
+    }
+    if (nrow(assembly_meta) != 1) {
+      if (verbose) message("Failed. Assembly metadata not appropriate.")
+      return(NA)
+    }
+    ftppath <- assembly_meta$ftppath
+    prefix <- strsplit(ftppath, "/")[[1]]
+    prefix <- prefix[length(prefix)]
+    suffix <- switch(type,
+                     assembly_report = ".txt",
+                     assembly_stats = ".txt",
+                     cds.fna = "_from_genomic.fna.gz",
+                     feature_count = ".txt.gz",
+                     feature_table = ".txt.gz",
+                     genomic.fna = ".gz",
+                     genomic.gbff = ".gz",
+                     genomic.gff = ".gz",
+                     genomic.gtf = ".gz",
+                     protein.faa = ".gz",
+                     protein.gpff = ".gz",
+                     translated_cds = ".faa.gz")
+    urlpath <- paste0(ftppath, "/", prefix, "_" ,type, suffix)
+    if (is.null(dirpath)) dirpath = getwd()
+    if (!dir.exists(dirpath)) dir.create(dirpath, recursive = TRUE)
+    filepath <- paste0(dirpath, "/", basename(urlpath))
+    if (file.exists(filepath)) {
+      if (verbose) message("Done. Already downloaded.")
+      return(NA)
+    }
+    out <- try(download.file(urlpath,
+                             destfile = filepath,
+                             quiet = TRUE), silent = TRUE)
     if (inherits(out, "try-error")) {
-      message("Download failed: ", urlpath)
+      if (verbose) message("Failed. Webservice temporarily down.")
       file.remove(filepath)
+      return(NA)
     }
+    message("Done.")
   }
+  out <- lapply(accessions, function(x) foo(x, typ = type))
 }
---
+++ R/ncbi_meta_assembly.R
@@ -0,0 +1,58 @@
+#' Retrieve NCBI Assembly metadata
+#' 
+#' @param assembly_uid numeric
+#' @examples 
+#' \dontrun{
+#' ncbi_meta_assembly(419738)
+#' }
+#' @export
+ncbi_meta_assembly <- function(assembly_uid) {
+  res <- rentrez::entrez_fetch(
+    db = "assembly", id = assembly_uid, rettype = "docsum", retmode = "xml")
+  parsed_xml <- XML::xmlParse(res)
+  rootnode <- XML::xmlRoot(parsed_xml)
+
+  assembly_accession <- XML::xpathSApply(
+    rootnode, "//AssemblyAccession", XML::xmlValue)
+  assembly_accession <- coerce_xmlvalue(assembly_accession)
+  asm_name <- XML::xpathSApply(
+    rootnode, "//AssemblyName", XML::xmlValue)
+  asm_name <- coerce_xmlvalue(asm_name)
+  bioproject <- XML::xpathSApply(
+    rootnode, "//BioprojectAccn", XML::xmlValue)
+  bioproject <- coerce_xmlvalue(bioproject)
+  biosample <- XML::xpathSApply(
+    rootnode, "//BioSampleAccn", XML::xmlValue)
+  biosample <- coerce_xmlvalue(biosample)
+  organism <- XML::xpathSApply(
+    rootnode, "//Organism", XML::xmlValue)
+  organism <- coerce_xmlvalue(organism)
+  assembly_status <- XML::xpathSApply(
+    rootnode, "//AssemblyStatus", XML::xmlValue)
+  assembly_status <- coerce_xmlvalue(assembly_status)
+  taxid <- XML::xpathSApply(
+    rootnode, "//Taxid", XML::xmlValue)
+  taxid <- coerce_xmlvalue(taxid)
+  ftppath <- ifelse(
+    grepl("^GCF", assembly_accession),
+    XML::xpathSApply(rootnode, "//FtpPath_RefSeq", XML::xmlValue),
+    XML::xpathSApply(rootnode, "//FtpPath_GenBank", XML::xmlValue))
+  ftppath <- coerce_xmlvalue(ftppath)
+  out <- tibble::tibble(
+    assembly_accession = assembly_accession,
+    asm_name = asm_name,
+    bioproject = bioproject,
+    biosample = biosample,
+    organism = organism,
+    assembly_status = assembly_status,
+    taxid = taxid,
+    ftppath = ftppath)
+  return(out)
+}
+
+coerce_xmlvalue <- function(x){
+  if (length(x) > 1) return(paste(x, collapse = "|"))
+  if (length(x) == 1 && x == "") return(NA)
+  if (length(x) == 0) return(NA)
+  return(x)
+}
