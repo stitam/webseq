b28e13f15a660a0f8e2d83f3cb6894695bba299a
Tamas Stirling <stirling.tamas@gmail.com>
2022-08-02 09:14:24 +0200

    Improve NA handling. Closes #7.

--- R/tidy_biosample.R
+++ R/tidy_biosample.R
@@ -1,7 +1,8 @@
 tidy_biosample <- function(biosample_df) {
   na_terms <- c(
     "missing", "na", "n/a", "none", "not applicable", "not collected", 
-    "not_determined", "unknown", "NA")
+    "not determined", "not provided", "null", "restricted access",
+    "unknown")
   out <- tibble::as_tibble(data.frame(lapply(biosample_df, function(x) {
     ifelse(tolower(x) %in% na_terms == FALSE, x, NA)
   })))
