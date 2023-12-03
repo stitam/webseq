4f751c45c99fb74cf76d4c839583cfe268f781a8
Tamas Stirling <stirling.tamas@gmail.com>
2023-12-03 14:11:00 +0100

    Add new biosample to examples

--- scripts/prep_examples.R
+++ scripts/prep_examples.R
@@ -6,7 +6,8 @@ examples <- list(
   biosample = c(
     "SAMN02714232", # contact email
     "SAMD00057211", # different ids, duplicated attributes
-    "SAMN32745369"
+    "SAMN32745369",
+    "SAMEA114639742"
   )
 )
 
