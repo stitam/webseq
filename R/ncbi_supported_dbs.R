c88b19d03faf36ab765716cda7cf4622955a3ea2
Tamas Stirling <stirling.tamas@gmail.com>
2023-12-11 16:38:45 +0100

    Cache ncbi db names within session

--- R/ncbi_supported_dbs.R
+++ R/ncbi_supported_dbs.R
@@ -1,4 +1,10 @@
 ncbi_supported_dbs <- function() {
-  dbs <- c("assembly", "bioproject", "biosample", "gene", "protein", "sra")
+  if (!dir.exists(tempdir())) dir.create(tempdir())
+  if (!file.exists(paste0(tempdir(), "/webseq.rds"))) {
+    dbs <- rentrez::entrez_dbs()
+    saveRDS(dbs, file = paste0(tempdir(), "/webseq.rds"))
+  } else {
+    dbs <- readRDS(paste0(tempdir(), "/webseq.rds"))
+  }
   return(dbs)
 }
