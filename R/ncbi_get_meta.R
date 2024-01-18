96231c161ff8848f4b83911b3db376b3acef6e17
Tamas Stirling <stirling.tamas@gmail.com>
2024-01-18 20:55:54 +0100

    Give 'ncbi_meta' class to unparsed metadata only

--- R/ncbi_get_meta.R
+++ R/ncbi_get_meta.R
@@ -1,37 +1,34 @@
 #' Get sequence metadata from NCBI
 #' 
-#' This function retrieves metadata from a given NCBI sequence database.
+#' This function retrieves sequence metadata from a given NCBI sequence
+#' database.
 #' @param query either an object of class \code{ncbi_uid} or an integer vector 
 #' of UIDs. See Details for more information.
 #' @param db character; the database to search in. For options see
-#' \code{ncbi_dbs()}. Not all databases are supported.
+#' \code{ncbi_dbs()}. Note that not all of these databases are supported.
 #' @param batch_size integer; the number of search terms to query at once. If
 #' the number of search terms is larger than \code{batch_size}, the search terms
 #' are split into batches and queried separately.
 #' @param use_history logical; should the function use web history for faster
 #' API queries?
-#' @param parse logical; Should the function attempt to parse the output into a
-#' tibble? If unsuccessful, the function will return the unparsed output.
+#' @param parse logical; should the function attempt to parse the output into a
+#' tibble?
 #' @param verbose logical; Should verbose messages be printed to console?
-#' @return The function returns a list with two elements:
-#' \itemize{
-#' \item \code{meta}: if \code{parse = TRUE} then either a tibble with the 
-#' metadata or if parsing is unsuccessful, the unparsed metadata. If
-#' \code{parse = FALSE} the unparsed metadata.
-#' \item \code{history}: a tibble of web histories.
-#' }
-#' @details Some functions in webseq, e.g. \code{ncbi_get_uid()} or
-#' \code{ncbi_link_uid()} return objects of class \code{"ncbi_uid"}. These
-#' objects may be used directly as query input for \code{ncbi_get_meta()}. This
-#' approach is recommended because the internal structure of these objects make
-#' \code{ncbi_get_meta()} queries more robust. Alternatively, you can also
-#' use a character vector of UIDs as query input.
-#' @details If query is a \code{"ncbi_uid"} object, the \code{db} argument is
-#' optional. If \code{db} is not specified, the function will use the
-#' \code{db} attribute of the \code{"ncbi_uid"} object as \code{db} argument.
-#' However, if it is specified, it must be identical to the \code{db} attribute
-#' of the \code{"ncbi_uid"} object. If query is a character vector, the
-#' \code{db} argument is required.
+#' @return If \code(parse = FALSE) the function will return an object of class
+#' \code{ncbi_meta}, which is a character vector with some extra information
+#' about the database. This output can be used directly with \code{ncbi_parse}.
+#' If \code{parse = TRUE} the function will attempt to parse the data using 
+#' \code{ncbi_parse}. If parsing is successful, the function will return a
+#' tibble, otherwise it will return the unparsed \code{ncbi_meta} object.
+#' @details You can give UIDs to \code{ncbi_get_meta()} in two ways: 1. You can
+#' use functions like \code{ncbi_get_uid()} or \code{ncbi_link_uid} to get UIDs,
+#' and then use the returned \code{ncbi_uid} objects directly with
+#' \code{ncbi_get_meta}. If you follow this approach then you do not have to
+#' specify the \code{db} argument since the function can extract it from the
+#' \code{ncbi_uid} object. However, if you do provide it, then it must be
+#' identical to the \code{db} attribute of the \code{"ncbi_uid"} object. 2. 
+#' Alternatively, you can just provide a vector of UIDs, but then you must 
+#' specify the \code{db} argument as well.
 #' @examples
 #' \dontrun{
 #' data(examples)
@@ -155,11 +152,14 @@ ncbi_get_meta <- function(
       foo_from_ids(x, db = db)
     })
   }
+  attr(res, "db") = db
+  class(res) <- c("ncbi_meta", class(res))
+  validate_webseq_class(res)
   if (parse) {
     if (verbose) {
       message("Attempting to parse retrieved metadata.")
     }
-    res_parsed <- ncbi_parse(meta = res, db = db, verbose = verbose)
+    res_parsed <- ncbi_parse(meta = res, verbose = verbose)
     if (all("data.frame" %in% class(res_parsed))) {
       out <- dplyr::bind_rows(res_parsed)
       if (verbose) message("Done.")
@@ -170,8 +170,5 @@ ncbi_get_meta <- function(
   } else {
     out <- res
   }
-  attr(out, "db") = db
-  class(out) <- c("ncbi_meta", class(out))
-  validate_webseq_class(out)
   return(out)
 }
