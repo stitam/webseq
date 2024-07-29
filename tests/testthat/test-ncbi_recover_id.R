e05c51a53a8a241ccacb967e9385a0e770a43c7c
Tamas Stirling <stirling.tamas@gmail.com>
2024-07-29 17:24:20 +0200

    More tests for ncbi_recover_id()

--- tests/testthat/test-ncbi_recover_id.R
+++ tests/testthat/test-ncbi_recover_id.R
@@ -21,3 +21,9 @@ test_that("ncbi_recover_id() works with duplicates", {
   
   expect_equal(ids, c("SAMN02597423", "SAMN02597423"))
 })
+
+test_that("ncbi_recover_id() works with nuccore", {
+  uid <- ncbi_get_uid("OP617744.1", db = "nuccore")
+  id <- ncbi_recover_id(uid)
+  expect_equal(id,"OP617744.1")
+})
