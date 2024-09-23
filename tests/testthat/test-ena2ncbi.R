8f221c26c1f55e51bc3bf85cb074ad354c43ebf1
Tamas Stirling <stirling.tamas@gmail.com>
2024-09-23 21:59:44 +0200

    Implement ID conversions between ENA studies and NCBI bioprojects

--- tests/testthat/test-ena2ncbi.R
+++ tests/testthat/test-ena2ncbi.R
@@ -1,5 +1,5 @@
 test_that("ena2ncbi() works with a single query", {
-  A <- ena2ncbi("ERS15941089")
+  A <- ena2ncbi("ERS15941089", type = "sample")
   expect_equal(dim(A), c(1,2))
   expect_equal(names(A), c("ena", "ncbi"))
   expect_equal(A$ena, "ERS15941089")
@@ -7,21 +7,21 @@ test_that("ena2ncbi() works with a single query", {
 })
 
 test_that("ena2ncbi() works with multiple queries", {
-  B <- ena2ncbi(c("ERS15941089", "ERS15939592"))
+  B <- ena2ncbi(c("ERS15941089", "ERS15939592"), type = "sample")
   expect_equal(dim(B), c(2,2))
   expect_equal(B$ena, c("ERS15941089", "ERS15939592"))
   expect_equal(B$ncbi, c("SAMEA113946840", "SAMEA113945342"))
 })
 
 test_that("ena2ncbi() returns results in the same order as the query", {
-  C <- ena2ncbi(c("ERS15939592", "ERS15941089"))
+  C <- ena2ncbi(c("ERS15939592", "ERS15941089"), type = "sample")
   expect_equal(C$ena, c("ERS15939592", "ERS15941089"))
   expect_equal(C$ncbi, c("SAMEA113945342", "SAMEA113946840"))
 })
 
 test_that("ena2ncbi() returns an empty data frame when given an NCBI ID", {
-  D <- suppressWarnings(ena2ncbi("SAMEA111452506"))
-  D_msg <- capture_warnings(ena2ncbi("SAMEA111452506"))
+  D <- suppressWarnings(ena2ncbi("SAMEA111452506", type = "sample"))
+  D_msg <- capture_warnings(ena2ncbi("SAMEA111452506", type = "sample"))
   expect_equal(dim(D), c(0,2))
   expect_equal(class(D$ena), "character")
   expect_equal(class(D$ncbi), "character")
@@ -29,8 +29,8 @@ test_that("ena2ncbi() returns an empty data frame when given an NCBI ID", {
 })
 
 test_that("ena2ncbi() removes invalid queries and returns the rest", {
-  E <- suppressWarnings(ena2ncbi(c("ERS15939592", "balloon", "ERS15941089")))
+  E <- suppressWarnings(
+    ena2ncbi(c("ERS15939592", "balloon", "ERS15941089"), type = "sample")
+  )
   expect_equal(dim(E), c(2,2))
 })
-
-
